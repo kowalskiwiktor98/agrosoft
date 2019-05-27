91d01ad8012230d90540f4b16032a7494fa86aae
kowalskiwiktor98 <wiktor9196667@gmail.com>
2019-05-27 23:42:33 +0200

    .

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+
+document.getElementById("loginbtn").onclick = zaloguj;
+
+
 async function zaloguj() {
     const username = document.getElementById('login').value;
     const password = document.getElementById('password').value;
-
+    var accessToken;
+    var tokenType;
     try {
+        console.log(username);
+        console.log(password);
         const response = await axios.post('http://mikey.ovh:8080/restAPI/api/auth/signin', {
             username,
             password
         });
-        console.log(response);
+        //console.log(response);
+        accessToken = response.data.accessToken;
+        tokenType = response.data.tokenType;
+        console.log(accessToken);
+        console.log(tokenType);
+        window.location.pathname = 'main.html';
+
     } catch (e) {
         console.error(e);
     }
+
+
 
 }
 
