5ae8913923f075044e9dadc3a13044a21c4201fb
kowalskiwiktor98 <wiktor9196667@gmail.com>
2019-06-07 19:45:37 +0200

    Display users in Manage

--- Manage.js
+++ Manage.js
@@ -12,8 +12,24 @@ async function getUsers() {
 
     $.ajax(settings).done(function (response) {
         console.log(response);
-        localStorage.setItem('users', response);
+        localStorage.setItem('users', JSON.stringify(response));
         //localStorage.setItem('users', response);
+        displayUsers();
     });
+
+}
+
+function displayUsers() {
+    var array = JSON.parse(localStorage.getItem('users'));
+    console.log(array);
+    //console.log(JSON.stringify(array));
+    for (user of array) {
+        console.log(user);
+        var li = document.createElement("li");
+        var t = document.createTextNode(user.first_name + ' ' + user.last_name);
+        li.appendChild(t);
+        document.getElementById("userList").appendChild(li);
+    }
+
 
 }
