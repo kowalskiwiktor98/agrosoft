35644a324400d2898a36e0df09a27fd7cf9ad6ea
kowalskiwiktor98 <wiktor9196667@gmail.com>
2019-06-09 14:00:01 +0200

    Add messages prototype

--- script/Messages.js
+++ script/Messages.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     getMessages();
+    displayinboxchat();
 };
 
 function getMessages() {
@@ -13,8 +14,27 @@ function getMessages() {
         }
     }
     $.ajax(settings).done(function (response) {
-        console.log("Wiadomości - " + JSON.stringify(response));
+        //console.log("Wiadomości - " + JSON.stringify(response));
         localStorage.setItem('messages', JSON.stringify(response));
         //localStorage.setItem('users', response);
     });
+}
+function displayinboxchat() {
+    var messages = JSON.parse(localStorage.getItem('messages'));
+    console.log(messages);
+    for (m of messages) {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        var sender = document.createElement("span");
+        var subject = document.createElement("span");
+        var text = document.createElement("span");
+        sender.appendChild(document.createTextNode(m.message.creator.first_name + ' ' + m.message.creator.last_name))
+        subject.appendChild(document.createTextNode(m.message.subject));
+        text.appendChild(document.createTextNode(m.message.body));
+        a.appendChild(sender);
+        a.appendChild(subject);
+        a.appendChild(text);
+        li.appendChild(a);
+        document.getElementById("mailbox").appendChild(li);
+    }
 }
